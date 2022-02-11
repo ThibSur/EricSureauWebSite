e2a4bf95bfbae7b2a4898e4879a22a7a85c82e59
ThibSur <97024917+ThibSur@users.noreply.github.com>
2022-02-11 10:26:47 +0100

    Delete com directory

--- com/websiteSureau/repository/SiteNewsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.websiteSureau.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.websiteSureau.model.SiteNews;
-
-public interface SiteNewsRepository extends JpaRepository <SiteNews, Integer> {
-	
-}
--- com/websiteSureau/service/EmailService.java
+++
@@ -1,5 +0,0 @@
-package com.websiteSureau.service;
-
-public interface EmailService {
-    void sendSimpleMessage(String to, String subject, String text);
-}
